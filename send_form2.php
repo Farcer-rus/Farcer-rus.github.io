8fcc5214fbf42727e275b2c7c9cb9adb98f74c45
Farcer-rus <Farcer-rus@users.noreply.github.com>
2019-11-08 15:29:35 +0300

    436

---
+++ send_form2.php
@@ -0,0 +1,11 @@
+<?php
+$fio= $_POST['fio'];
+$phone= $_POST['phone'];
+$email= $_POST['email'];
+$adres= $_POST['adres'];
+$emailTo = 'zanoskoai@gmail.com'; //—юда введите ¬аш email
+$body = "$fio \n\n$phone\n\n$email" \n\n$adres;
+$headers = "Content-Type: text/plain; charset=utf-8\r\n".'From: MySite <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $emailTo;
+mail($emailTo, $fio, $body, $headers);
+$emailSent = true;
+?>
