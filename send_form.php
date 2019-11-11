dd9121b0f722d69cdb4d5d3c10736b2667b96e16
Farcer-rus <56567045+Farcer-rus@users.noreply.github.com>
2019-11-11 23:53:09 +0300

    Update send_form.php

--- send_form.php
+++ send_form.php
@@ -3,7 +3,7 @@ $fio= $_POST['fio'];
 $phone= $_POST['phone'];
 $email= $_POST['email'];
 $adres= $_POST['adres'];
-$emailTo = '123428@bk.ru'; //Сюда введите Ваш email
+$emailTo = 'zanoskoai@gmail.com'; //Сюда введите Ваш email
 $body = "$fio \n\n$phone\n\n$email \n\n$adres";
 $headers = "Content-Type: text/plain; charset=utf-8\r\n".'From: MySite <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $emailTo;
 mail($emailTo, $fio, $body, $headers);
